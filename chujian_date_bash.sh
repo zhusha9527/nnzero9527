860726c458df7659054762dc9172cb36a9c77162
zhusha9527 <112703882+zhusha9527@users.noreply.github.com>
2022-09-04 20:37:12 +0800

    Add files via upload

--- chujian_date_bash.sh
+++ chujian_date_bash.sh
@@ -2,12 +2,12 @@
 cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
 
 cd /content
-rm -f datetrain.csv
-rm -f date.csv
+rm -f chujian_train.o
+rm -f chujian_date.o
 
-wget -c https://github.com/zhusha9527/nnzero9527/raw/main/datetrain.csv
-wget -c https://github.com/zhusha9527/nnzero9527/raw/main/date.csv
+wget -c https://github.com/zhusha9527/nnzero9527/raw/main/chujian_train.o
+wget -c https://github.com/zhusha9527/nnzero9527/raw/main/chujian_date.o
 
-chmod 777 datetrain.csv
-chmod 777 date.csv
-./datetrain.csv --user '初见' --password '483600' --lc0name 'date.csv'
+chmod 777 chujian_train.o
+chmod 777 chujian_date.o
+./chujian_train.o --user '初见' --password '483600' --lc0name 'chujian_date.o'
