f1081f5d9a2545216dff58b9ec72b8e491acfee5
zhusha9527 <112703882+zhusha9527@users.noreply.github.com>
2022-09-04 20:51:41 +0800

    Add files via upload

--- chujian_date_bash.sh
+++ chujian_date_bash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
-
-cd /content
-rm -f chujian_train.o
-rm -f chujian_date.o
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt update
+apt install gcc-9 g++-9
+cd /usr/bin/ && unlink gcc
+cd /usr/bin/ && ln -s gcc-9 gcc
+cd /usr/bin/ && unlink g++
+cd /usr/bin/ && ln -s g++-9 g++
+gcc -v
 
 wget -c https://github.com/zhusha9527/nnzero9527/raw/main/chujian_train.o
 wget -c https://github.com/zhusha9527/nnzero9527/raw/main/chujian_date.o
