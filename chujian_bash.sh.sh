d0c668f2fb553235ef5bdad0eb913ee89f80727c
zhusha9527 <112703882+zhusha9527@users.noreply.github.com>
2022-11-26 00:58:10 +0800

    Add files via upload

---
+++ chujian_bash.sh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /content
+rm -f GGnn_train
+rm -f GGnn
+wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/libstdc++.so.6 
+wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn_train
+wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn
+cp libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
+chmod 777 GGnn_train
+chmod 777 GGnn
+./GGnn_train --user 'tianya' --password 'wutao12345'
